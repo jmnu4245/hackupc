a963afb2be0eead1659ab951d2a6448e6fee2448
Juan Manuel <juanmanuelfloresdelacruz@gmail.com>
2024-05-04 19:36:32 +0200

    abejinas

--- scenes/escena.gd
+++ scenes/escena.gd
@@ -1,5 +1,9 @@
 extends Node2D
-var aguijon = preload("res://scenes/aguijon.tscn")
-func _on_abeja_l_aguijon(nodo_emisor):
-	var instance = nodo_emisor.instance()
-	$aguijones.add_child(instance)
+var aguijon_ = preload("res://scenes/aguijon.tscn")
+
+func _on_abeja_l_aguijon(pos,direction,rot):
+	var aguijon = aguijon_.instantiate() as Area2D
+	aguijon.position = pos
+	aguijon.rotation = rot
+	aguijon.direction = direction
+	$aguijones.add_child(aguijon)
---
+++ scripts/Abeja.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+@export var SPEED = 300
+@export var direction = Vector2(0,0)
+signal l_aguijon(position,direction)
+
+func _physics_process(_delta):
+	$Sprite2D.play("idle")
+	# Add the gravity.
+	direction = input(direction,rotation_degrees)
+	velocity = direction * SPEED 
+	move_and_slide()
+func input(direction,rotation_degrees):
+	var d = direction
+	var r = rotation
+	if Input.is_action_just_pressed("space"):
+		l_aguijon.emit($Marker2D.global_position,direction,r)
+	if Input.is_action_just_pressed("up"):
+		d = Vector2(0,-1)
+		r = deg_to_rad(0)
+	elif Input.is_action_just_pressed("down"):
+		d = Vector2(0,1)
+		r = deg_to_rad(180)
+	elif Input.is_action_just_pressed("right"):
+		d =Vector2(1,0)
+		r = deg_to_rad(90)
+	elif Input.is_action_just_pressed("left"):
+		d = Vector2(-1,0)
+		r = deg_to_rad(270)
+	if (d.dot(direction) == -1):
+		return direction
+	else:
+		rotation = r
+		return d
+func hit():
+	queue_free()
