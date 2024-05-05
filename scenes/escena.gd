03663b784064717c9b9179c3ec52ca64100c64fe
Juan Manuel <juanmanuelfloresdelacruz@gmail.com>
2024-05-05 02:46:55 +0200

    va

--- scenes/escena.gd
+++ scenes/escena.gd
@@ -1,11 +1,10 @@
 extends Node2D
-var aguijon_ = preload("res://scenes/aguijon.tscn")
-var mapa1 = preload("res://mapas/mapa1.tscn")
-var mapa2 = preload("res://mapas/mapa2.tscn")
-var mapa3 = preload("res://mapas/mapa3.tscn")
+var aguijon_ = load("res://scenes/aguijon.tscn")
+
 func _ready():
 	randomize()
-	var map = elegir_mapa_al_azar()
+	var string = elegir_mapa_al_azar()
+	var map = load(string)
 	get_tree().get_root().add_child(map.instantiate())
 const radioCS = 23
 var near = true
@@ -17,9 +16,9 @@ func _on_abeja_l_aguijon(pos,direction,rot):
 	aguijon.direction = direction
 	$aguijones.add_child(aguijon)
 func elegir_mapa_al_azar():
-	var mapas = [mapa1, mapa2, mapa3]
-	var indice_aleatorio = randi() % mapas.size()
-	return mapas[indice_aleatorio]
+	var i = randi() % 3
+	var string = "res://mapas/mapa"+ str(i) +".tscn"
+	return string
 
 func _process(delta):
 	var pos1 = $Abeja.position
