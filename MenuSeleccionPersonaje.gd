1c8c91fe708c3ed73c45ae72aeaaa0deba7cfede
Juan Manuel <juanmanuelfloresdelacruz@gmail.com>
2024-05-05 09:02:06 +0200

    `p

--- MenuSeleccionPersonaje.gd
+++ MenuSeleccionPersonaje.gd
@@ -1,31 +1,11 @@
 extends Control
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_texture_button_pressed():
-	pass # Replace with function body.
-
-
-func _on_texture_button_2_pressed():
-	pass # Replace with function body.
-
-
-func _on_texture_button_3_pressed():
-	pass # Replace with function body.
-
-
-func _on_texture_button_4_pressed():
-	pass # Replace with function body.
-
+var last_pressed_button = null
 
 func _on_button_pressed():
 		get_tree().change_scene_to_file("res://Menu.tscn")
