6fbfa8e3ce3e1a6105f9548624bfc5b0770e2815
Juan Manuel <juanmanuelfloresdelacruz@gmail.com>
2024-05-05 11:15:39 +0200

    ds

--- MenuSeleccionPersonaje.gd
+++ MenuSeleccionPersonaje.gd
@@ -2,9 +2,10 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 signal spriteabejas(abeja,abeja2)
 var abeja = 1
-var abeja2 =5
+var abeja2 =1
 var last_pressed_button = null
 func _on_button_pressed():
+	save(abeja,abeja2)
 	get_tree().change_scene_to_file("res://Menu.tscn")
 func _on_texture_button_1_pressed():
 	abeja=1
@@ -74,3 +75,6 @@ func desactivar_botones1(i: int) -> void:
 			$TextureButton6.button_pressed = false
 			$TextureButton7.button_pressed = false
 			$TextureButton8.button_pressed = true
+func save(abeja,abeja2):
+	var my_singleton = get_node("variables")
+	my_singleton.setabejas(abeja,abeja2)
